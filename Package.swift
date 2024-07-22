83e033a9e5536de8eb3adc3c5fc732ee63a39c56
Theo Madzou <madztheo@hotmail.fr>
2024-07-22 21:47:50 +0200

    support variable-sized local SRS files

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/madztheo/SwoirCore.git", exact: "0.4.0"),
-        .package(url: "https://github.com/madztheo/Swoirenberg.git", exact: "0.30.0-8"),
+        .package(url: "https://github.com/madztheo/Swoirenberg.git", exact: "0.30.0-9"),
     ],
     targets: [
         .target(
