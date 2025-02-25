e335bccfee130f7644e4b3e72bca472f18fe849f
Theo Madzou <madztheo@hotmail.fr>
2025-02-25 22:27:25 +0000

    update to Noir 1.0.0 beta 3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swoir/SwoirCore.git", exact: "0.7.0"),
-        .package(url: "https://github.com/Swoir/Swoirenberg.git", exact: "1.0.0-beta.1-2"),
+        .package(url: "https://github.com/Swoir/Swoirenberg.git", exact: "1.0.0-beta.3-1"),
     ],
     targets: [
         .target(
