a1a4dfc8dc336c211797db1f8dc9632ae85387cc
Theo Madzou <madztheo@hotmail.fr>
2025-07-11 15:46:33 +0100

    update Swoirenberg to 1.0.0-beta.7-2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swoir/SwoirCore.git", exact: "0.9.0"),
-        .package(url: "https://github.com/Swoir/Swoirenberg.git", exact: "1.0.0-beta.7-1"),
+        .package(url: "https://github.com/Swoir/Swoirenberg.git", exact: "1.0.0-beta.7-2"),
     ],
     targets: [
         .target(
