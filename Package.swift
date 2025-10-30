a49e7399e8e55728bd09338257425597aee74b82
Theo Madzou <madztheo@hotmail.fr>
2025-10-30 16:32:12 +0000

    apply bb patch

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swoir/SwoirCore.git", exact: "0.11.0"),
-        .package(url: "https://github.com/Swoir/Swoirenberg.git", exact: "1.0.0-beta.14-1"),
+        .package(url: "https://github.com/Swoir/Swoirenberg.git", exact: "1.0.0-beta.14-2"),
     ],
     targets: [
         .target(
